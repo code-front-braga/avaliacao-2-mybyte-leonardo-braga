2580cc74ca40c8e7a66ac840d4adf2066c5e1074
Leonardo Braga <programer_frontbraga@outlook.com>
2025-09-14 16:15:07 -0300

    feat: add simple interest calculation component

---
+++ src/components/simple-interest.tsx
@@ -0,0 +1,29 @@
+import { simpleInterestAmountFunction } from "../helpers/simple-interest-amount-function"
+import { simpleInterestFunction } from "../helpers/simple-interest-function"
+import CustomInterestResult from "./custom-interest-result"
+
+const SimpleInterest = () => {
+  const time = 8
+  const rate = 0.05
+  const capital = 2000
+
+  const simpleInterest = simpleInterestFunction({
+    capital,
+    rate,
+    time,
+  })
+
+  const amount = simpleInterestAmountFunction({ capital, simpleInterest })
+
+  return (
+    <CustomInterestResult
+      time={time}
+      rate={rate}
+      amount={amount}
+      capital={capital}
+      interest={simpleInterest}
+    />
+   );
+}
+
+export default SimpleInterest;
